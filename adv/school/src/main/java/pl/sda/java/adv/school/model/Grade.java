c241689d1ff157334a8ad18fb8b7b1e9c00c541d
Walerij <vvoinarovych@gmail.com>
2022-02-15 14:02:34 +0100

    added gradeUtils

--- adv/school/src/main/java/pl/sda/java/adv/school/model/Grade.java
+++ adv/school/src/main/java/pl/sda/java/adv/school/model/Grade.java
@@ -3,9 +3,51 @@ package pl.sda.java.adv.school.model;
 import java.math.BigDecimal;
 
 public class Grade {
+    private String studentId;
+    private String subjectCode;
+    private GradeWeight gradeWeight;
+    private BigDecimal value;
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getSubjectCode() {
+        return subjectCode;
+    }
+
+    public void setSubjectCode(String subjectCode) {
+        this.subjectCode = subjectCode;
+    }
+
+    public GradeWeight getGradeWeight() {
+        return gradeWeight;
+    }
+
     public void setGradeWeight(GradeWeight gradeWeight) {
+        this.gradeWeight = gradeWeight;
+    }
+
+    public BigDecimal getValue() {
+        return value;
     }
 
     public void setValue(BigDecimal value) {
+        this.value = value;
     }
+
+    @Override
+    public String toString() {
+        return "Grade{" +
+                "studentId='" + studentId + '\'' +
+                ", subjectCode='" + subjectCode + '\'' +
+                ", gradeWeight=" + gradeWeight +
+                ", value=" + value +
+                '}';
+    }
+
 }
--- adv/school/src/main/java/pl/sda/java/adv/school/util/GradeUtils.java
+++ adv/school/src/main/java/pl/sda/java/adv/school/util/GradeUtils.java
@@ -3,6 +3,7 @@ package pl.sda.java.adv.school.util;
 import pl.sda.java.adv.school.model.Grade;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Collection;
 import java.util.Optional;
 
@@ -12,6 +13,18 @@ public class GradeUtils {
     }
 
     public static Optional<BigDecimal> gradesAverage(Collection<Grade> grades) {
-        return Optional.empty();
+        BigDecimal sumOfGrades = grades.stream()
+                .map(t -> t.getValue().multiply(t.getGradeWeight().getValue()))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        BigDecimal sumOfWeightFactors = grades.stream()
+                .map(t -> t.getGradeWeight().getValue())
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        if (sumOfGrades.compareTo(BigDecimal.ONE) < 0 || sumOfWeightFactors.compareTo(BigDecimal.ZERO) <= 0) {
+            return Optional.empty();
+        }
+
+        return Optional.of(sumOfGrades.divide(sumOfWeightFactors, 2, RoundingMode.HALF_UP));
     }
 }
